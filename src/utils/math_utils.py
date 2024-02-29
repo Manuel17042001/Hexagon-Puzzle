82cb94857d453e301ea83f573319d65c232821db
Manuel Reichegger <manuel.reichegger@student.uibk.ac.at>
2024-02-29 18:09:45 +0100

    moved get_neighbour_coordinates to math_utils.py

--- src/utils/math_utils.py
+++ src/utils/math_utils.py
@@ -77,3 +77,41 @@ def get_corner_points_of_hexagon_facing_to_side(center: tuple[float, float], sid
         y_point = y + side_length * math.sin(angle_radians)
         points.append((int(x_point), int(y_point)))
     return points
+
+
+def get_neighbour_coordinates(x: int, y: int, n: int):
+    """
+    Calculates the coordinates of a neighboring cell in a hexagonal grid.
+
+    :param x: Current x-coordinate.
+    :param y: Current y-coordinate.
+    :param n: Neighbor index (0 to 5 inclusive).
+
+    :return: Coordinates of the neighboring cell as a tuple (int, int).
+
+    :raises AssertionError: If the neighbor index is not in the range [0, 5].
+    """
+    assert 0 <= n <= 5, "The index of the neighbours has to be between 0 and 5"
+
+    even_row_neighbors = {
+        0: (x - 1, y - 1),
+        1: (x, y - 1),
+        2: (x + 1, y),
+        3: (x, y + 1),
+        4: (x - 1, y + 1),
+        5: (x - 1, y)
+    }
+
+    odd_row_neighbors = {
+        0: (x, y - 1),
+        1: (x + 1, y - 1),
+        2: (x + 1, y),
+        3: (x + 1, y + 1),
+        4: (x, y + 1),
+        5: (x - 1, y)
+    }
+
+    if y % 2 == 0:
+        return even_row_neighbors[n]
+    else:
+        return odd_row_neighbors[n]
